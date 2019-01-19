ac31491c09087fc20c09bed6ffddd3c4bf93ecbf
Andrew <amccan5@uwo.ca>
2019-01-19 12:41:33 -0500

    Refactor, pattern, app to vision.

--- speech.py
+++ speech.py
@@ -34,100 +34,97 @@ from google.cloud.speech import types
 import pyaudio
 from six.moves import queue
 
-from MicrophoneStream import MicrophoneStream
+from microphoneStream import MicrophoneStream
 
 # Audio recording parameters
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
 FILLER_WORDS = ['basically', 'really', 'like', 'actually', 'very', 'literally', 'stuff', 'things', 'obviously', 'yeah']
 
-def listen_print_loop(responses):
-    """Iterates through server responses and prints them.
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-    """
-    num_chars_printed = 0
-    for response in responses:
-        if not response.results:
-            continue
+class SpeechHandler(object):
+    def listen_print_loop(self, responses):
+        """Iterates through server responses and prints them.
+        The responses passed is a generator that will block until a response
+        is provided by the server.
+        Each response may contain multiple results, and each result may contain
+        multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+        print only the transcription for the top alternative of the top result.
+        In this case, responses are provided for interim results as well. If the
+        response is an interim one, print a line feed at the end of it, to allow
+        the next result to overwrite it, until the response is a final one. For the
+        final one, print a newline to preserve the finalized transcription.
+        """
+        num_chars_printed = 0
+        for response in responses:
+            if not response.results:
+                continue
 
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
-        result = response.results[0]
-        if not result.alternatives:
-            continue
+            # The `results` list is consecutive. For streaming, we only care about
+            # the first result being considered, since once it's `is_final`, it
+            # moves on to considering the next utterance.
+            result = response.results[0]
+            if not result.alternatives:
+                continue
 
-        # Display the transcription of the top alternative.
-        transcript = result.alternatives[0].transcript
+            # Display the transcription of the top alternative.
+            transcript = result.alternatives[0].transcript
 
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
-        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+            # Display interim results, but with a carriage return at the end of the
+            # line, so subsequent lines will overwrite them.
+            #
+            # If the previous result was longer than this one, we need to print
+            # some extra spaces to overwrite the previous result
+            overwrite_chars = ' ' * (num_chars_printed - len(transcript))
 
-        if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + '\r')
-            sys.stdout.flush()
+            if not result.is_final:
+                sys.stdout.write(transcript + overwrite_chars + '\r')
+                sys.stdout.flush()
 
-            num_chars_printed = len(transcript)
+                num_chars_printed = len(transcript)
 
-        else:
-            print(transcript + overwrite_chars)
+            else:
+                print(transcript + overwrite_chars)
 
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            print(fillerStats(transcript))
+                # Exit recognition if any of the transcribed phrases could be
+                # one of our keywords.
+                self._socket.emit('newnumber', {'number': self.fillerStats(transcript)}, namespace='/test')
 
-            if re.search(r'\b(exit|quit)\b', transcript, re.I):
-                print('Exiting..')
-                break
+                if re.search(r'\b(exit|quit)\b', transcript, re.I):
+                    print('Exiting..')
+                    break
 
-            num_chars_printed = 0
+                num_chars_printed = 0
 
-def fillerStats(transcript):
-    filler_word_count = 0
-    for word in FILLER_WORDS:
-        filler_word_count = filler_word_count + transcript.count(word)
-    return filler_word_count / len(transcript.split()) * 100
+    def fillerStats(self, transcript):
+        filler_word_count = 0
+        for word in FILLER_WORDS:
+            filler_word_count = filler_word_count + transcript.count(word)
+        return filler_word_count / len(transcript.split()) * 100
 
-def main():
-    # See http://g.co/cloud/speech/docs/languages
-    # for a list of supported languages.
-    language_code = 'en-US'  # a BCP-47 language tag
+    def __init__(self, socket):
+        # See http://g.co/cloud/speech/docs/languages
+        # for a list of supported languages.
+        self._socket = socket
+        language_code = 'en-US'  # a BCP-47 language tag
 
-    client = speech.SpeechClient()
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=RATE,
-        language_code=language_code)
-    streaming_config = types.StreamingRecognitionConfig(
-        config=config,
-        interim_results=True)
+        client = speech.SpeechClient()
+        config = types.RecognitionConfig(
+            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=RATE,
+            language_code=language_code)
+        streaming_config = types.StreamingRecognitionConfig(
+            config=config,
+            interim_results=True)
 
-    with MicrophoneStream(RATE, CHUNK) as stream:
-        audio_generator = stream.generator()
-        requests = (types.StreamingRecognizeRequest(audio_content=content)
-                    for content in audio_generator)
+        with MicrophoneStream(RATE, CHUNK) as stream:
+            audio_generator = stream.generator()
+            requests = (types.StreamingRecognizeRequest(audio_content=content)
+                        for content in audio_generator)
 
-        responses = client.streaming_recognize(streaming_config, requests)
+            responses = client.streaming_recognize(streaming_config, requests)
 
-        # Now, put the transcription responses to use.
-        try:
-            listen_print_loop(responses)
-        except Exception as exception:
-            print("Exception handle: Exceeded maximum allowed stream duration of 65 seconds")
-
-
-if __name__ == '__main__':
-    main()
-# [END speech_transcribe_streaming_mic]
+            # Now, put the transcription responses to use.
+            try:
+                self.listen_print_loop(responses)
+            except Exception as exception:
+                print(exception)
