eaeb7743eef8b0600830a3ab608debc84bbdb449
rrangith <rrangith@edu.uwaterloo.ca>
2019-01-19 01:08:02 -0500

    initial flask app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return 'hi'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000, debug = True)
