b04fd39d8aa9c3aaefb4d3ce9ec32db0fb7c7928
Andrew <amccan5@uwo.ca>
2019-01-19 14:09:02 -0500

    Factor vision.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from threading import Thread, Event
 import subprocess
 
 from speech import SpeechHandler
+from vision import VisionHandler
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -33,6 +34,25 @@ class SpeechThread(Thread):
     def run(self):
         SpeechHandler(socketio)
 
+vision_thread = Thread()
+vision_thread_stop_event = Event()
+
+class VisionThread(Thread):
+    def __init__(self):
+        self.delay = 1
+        super(VisionThread, self).__init__()
+
+    # def speechStreaming(self):
+
+    #     while not speech_thread_stop_event.isSet():
+    #         number = round(random()*10, 3)
+    #         print(number)
+    #         socketio.emit('newnumber', {'number': number}, namespace='/test')
+    #         sleep(self.delay)
+
+    def run(self):
+        VisionHandler()
+
 
 @app.route('/')
 def index():
@@ -43,6 +63,7 @@ def index():
 def test_connect():
     # need visibility of the global thread object
     global speech_thread
+    global vision_thread
     print('Client connected')
 
     #Start the random number generator thread only if the thread has not been started before.
@@ -50,6 +71,10 @@ def test_connect():
         print("Starting Thread")
         speech_thread = SpeechThread()
         speech_thread.start()
+    if not vision_thread.isAlive():
+        print("Starting Thread")
+        vision_thread = VisionThread()
+        vision_thread.start()
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
