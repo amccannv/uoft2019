ee7240a9abf948b8059635f8b21f08e02b6fa538
rrangith <rrangith@edu.uwaterloo.ca>
2019-01-20 00:46:52 -0500

    adding stop logic and refactoring app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-# from flask_socketio import SocketIO, emit
-from flask import Flask, render_template, url_for, copy_current_request_context
-from random import random
-from time import sleep
 from threading import Thread, Event
-import subprocess
-import multiprocessing
-from multiprocessing import Queue, Pool
+from multiprocessing import Pool
+
+from flask import Flask, render_template
 
 from speech import SpeechHandler
 from vision import VisionHandler
@@ -16,6 +12,9 @@ app.config['DEBUG'] = True
 speech_thread = Thread()
 speech_thread_stop_event = Event()
 
+vision_thread = Thread()
+vision_thread_stop_event = Event()
+
 class SpeechThread(Thread):
     def __init__(self):
         self.delay = 1
@@ -23,9 +22,6 @@ class SpeechThread(Thread):
 
     def run(self):
         SpeechHandler()
-
-vision_thread = Thread()
-vision_thread_stop_event = Event()
 
 class VisionThread(Thread):
     def __init__(self):
@@ -35,29 +31,39 @@ class VisionThread(Thread):
     def run(self):
         VisionHandler()
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/start')
+@app.route('/start', methods=['POST'])
 def start():
     global speech_thread
     global vision_thread
-    
+
     if not speech_thread.isAlive():
-        print("Starting Thread")
+        print("Starting Speech Thread")
         speech_thread = SpeechThread()
         speech_thread.start()
     if not vision_thread.isAlive():
-        print("Starting Thread")
+        print("Starting Vision Thread")
         vision_thread = VisionThread()
         vision_thread.start()
 
-@app.route('/stop')
+    return "Started Threads"
+
+@app.route('/stop', methods=['POST'])
 def stop():
-    print('hold')
-    # stop logic
-    # render summary
+    global speech_thread
+    global vision_thread
+
+    if speech_thread.isAlive():
+        print("Stopping Speech Thread")
+        speech_thread.stop()
+    if vision_thread.isAlive():
+        print("Stopping Vision Thread")
+        vision_thread.stop()
+
+    return "Stopped Threads"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
